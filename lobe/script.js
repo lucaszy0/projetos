7fab9655d12315b826ca106b35e8e12322a7923c
lucas gabriel <gabriel.cardosso13@gmail.com>
2025-04-23 00:46:27 -0300

    3.0

---
+++ lobe/script.js
@@ -0,0 +1,21 @@
+gsap.from(".TextAnimation",{
+    
+    y:100,
+    opacity: 0,
+    scale: 1,
+    duration:2,
+    ease: "power1.out"
+    
+})
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".text", {
+  scrollTrigger: {
+    trigger: ".text",      // elemento que vai acionar a animação
+    start: "top 80%",    
+    toggleActions: "play none none none", // anima só uma vez
+  },
+  opacity: 0,
+  y: 100,
+  duration: 1
+});
